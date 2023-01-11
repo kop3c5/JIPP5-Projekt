398f7c76f24751f49914fbcf992a2cc291f89bd3
kop3c <kop3c5@gmail.com>
2023-01-11 16:55:10 +0100

    create new repo +model +controller

---
+++ ModeleDB/ModeleDB/Controllers/ModeleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModeleDB.Controllers
+{
+    public class ModeleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ModeleDB/ModeleDB/Models/ModeleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModeleDB.Models
+{
+    public class ModeleModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Scale { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Details { get; set; }
+    }
+}
