675a7d0508f2881b40006e21412f550ae18d4d6a
kop3c <kop3c5@gmail.com>
2023-01-11 18:17:14 +0100

    DB connection + init migration

---
+++ ModeleDB/ModeleDB/Migrations/20230111170940_Init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModeleDB.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Scale = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Models");
+        }
+    }
+}
---
+++ ModeleDB/ModeleDB/ModelContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModeleDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModeleDB
+{
+    public class ModelContext : DbContext
+    {
+
+        public DbSet<ModeleModel> Models { get; set; }
+
+        protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ModeleDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            optionsBuilder.UseSqlServer(cs);
+        }
+    }
+}
